7ded049af1134b06156f7ad4a51830b6f9ab73cc
Donald Smith III <donaldsmithiii@Donalds-MacBook.local>
2018-07-28 12:52:08 -0400

    "Learn More" button text added

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Donald Smith III | Avid Learner',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h2>UI / UX Designer</h2>
+    <h2>Full-Stack Developer</h2>
+    <Link to="/page-2/">Learn More</Link>
+  </div>
+)
+
+export default IndexPage
