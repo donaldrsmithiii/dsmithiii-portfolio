d95b4a4dd574fc351c76d7fd043aabe4f9f5ecbc
Donald Smith III <donaldsmithiii@Donalds-MacBook.local>
2018-07-29 10:30:51 -0400

    Added Hero Image to HTML

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <img src="../../img/Hero Logo.png" alt=""/>
     <h2>UI / UX Designer</h2>
     <h2>Full-Stack Developer</h2>
     <Link to="/page-2/">Learn More</Link>
