7a74c1a7efb13281d27128092b3c35b433e11189
Donald Smith III <donaldsmithiii@Donalds-MacBook.local>
2018-07-29 13:48:24 -0400

    Updated Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,23 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <img src="../../img/Hero Logo.png" alt=""/>
-    <h2>UI / UX Designer</h2>
-    <h2>Full-Stack Developer</h2>
-    <Link to="/page-2/">Learn More</Link>
+    Test
+    <div className="hero"></div>
+      <div className="hero--centerlogo"></div>
+      <div className="herogroup">
+        <h2>
+          <span className="herogroup--toptext">UI / UX</span>
+          <br/>
+          <span className="herogroup--bottomtext">Designer</span>
+        </h2>
+        
+        <h2>
+          <span className="herogroup--toptext">Full-Stack</span>
+          <br/>
+          <span className="herogroup--bottomtext">Developer</span>
+        </h2>
+      </div>
+      <Link classname="hero--learnmore" to="/page-2/">Learn More</Link>
   </div>
 )
 
